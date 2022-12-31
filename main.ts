0cbc3e3b55541ee68d5011a725d5f7546e893c56
Aravind <codesculptures@gmail.com>
2022-12-31 19:10:20 +0530

    Init

---
+++ main.ts
@@ -0,0 +1,7 @@
+import express from "npm:express@4.18";
+
+// import express from "./node_modules/.deno/express@4.18.2/node_modules/express/index.js";
+
+
+console.log("NOT BAD");
+console.log(express);
